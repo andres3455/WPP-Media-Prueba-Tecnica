0149f6d918658e4e9b69f369e67daebbb197fb35
andres Rodriguez <rodriguezandres160918@gmail.com>
2026-01-14 18:20:13 -0500

    Fixes on Dinamic Banner

--- DinamyBannerChallenge/js/main.js
+++ DinamyBannerChallenge/js/main.js
@@ -20,11 +20,10 @@ async function loadBanner() {
   } catch (error) {
     console.error("❌ Error al cargar banner:", error);
     document.getElementById("banner-title").textContent = "Error al cargar el anuncio";
+    document.getElementById("banner-desc").textContent = "Error al cargar descripción";
   }
 }
 
-// -------------------------------
-// Polling cada 10 segundos
-// -------------------------------
+
 loadBanner(); // Carga inicial
 setInterval(loadBanner, 10000); // Cada 10 segundos
